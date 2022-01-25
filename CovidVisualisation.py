d62951b92cbbeef057e300ddc23c78be8538267a
dknight26275 <dknight26275@gmail.com>
2022-01-26 02:20:40 +1000

    csv files updated, covid_cleaned now includes continent data.  visualization script still progressing

--- CovidVisualisation.py
+++ CovidVisualisation.py
@@ -1,5 +1,3 @@
-
-
 # imports/Initialization
 # import dash
 # from dash import dash_table
@@ -10,6 +8,7 @@
 import plotly.express as px
 import plotly.graph_objects as go
 import plotly.figure_factory as ff
+import plotly.io as pio
 from plotly.subplots import make_subplots
 import plotly as py
 # py.offline.init_notebook_mode(connected=True)
@@ -25,20 +24,55 @@ import random
 from datetime import timedelta
 import os
 
-
-#%%
+# %%
 # load cleaned/processed data
-covid_cleaned_df = pd.read_csv('H:/Covid19Dashboard/covid_cleaned.csv', parse_dates=['Date'])
-country_daily_df = pd.read_csv('H:/Covid19Dashboard/country_daily.csv', parse_dates=['Date'])
+covid_cleaned_df = pd.read_csv('H:/Covid19Dashboard/covid_cleaned.csv')
+country_daily_df = pd.read_csv('H:/Covid19Dashboard/country_daily.csv')
 country_latest_df = pd.read_csv('H:/Covid19Dashboard/country_latest.csv')
-daily_total_df = pd.read_csv('H:/Covid19Dashboard/daily_total.csv', parse_dates=['Date'])
+daily_total_df = pd.read_csv('H:/Covid19Dashboard/daily_total.csv')
 
 print(f'covid_cleaned: {covid_cleaned_df.info()}')
 print(f'country_daily{country_daily_df.info()}')
 print(f'country_latest: {country_latest_df.info()}')
-print(f'daily_toal: {daily_total_df.info()}')
+print(f'daily_total: {daily_total_df.info()}')
 
-#%%
+# %%
 '''Worldwide total confirmed cases and deaths'''
 # Remove null values from Province_State column of covid_cleaned_df
 covid_cleaned_df['Province_State'] = covid_cleaned_df['Province_State'].fillna('')
+df = covid_cleaned_df[covid_cleaned_df['Country_Region'] == 'Australia']
+bar = px.bar(df, x='Date'  # try settin   to Province_State for animation
+             , y='Confirmed'
+             , color='Provin ce_State'
+             , opacity=0.9
+             , orientation=
+             'v'
+             , barmode='relative'
+             , title='Confirmed Covid-19 cases in Australia'
+             , template='plotly_dark'
+             # ,animation_frame='Date', # need to sort the Date column properly for this to work
+             # ,range_y=[0,750000]
+             )
+
+bar.show()
+
+'''
+
+
+
+Also look at Racing bargraph animation, could look cool
+(Not necessary for function, but maybe also look into prettify or something similar for pycharm
+'''
+
+#%%
+
+
+df = pd.read_csv('H:/Covid19Dashboard/daily_total.csv')
+# can convert str date cols to datetime
+df['Date'] = pd.to_datetime(df['Date'], infer_datetime_format=True)
+# group by week
+df = df.groupby(pd.Grouper(key='Date', axis=0, freq='W'))['Confirmed','Deaths','New_cases','Deaths_per_100'].agg(
+    {'Confirmed':'max','Deaths':'max','New_cases':'sum','Deaths_per_100':'mean'})
+print(df.head(10))
+
+#%%
